5c2d890b0639b48eb00670bf64e9d66c77e181d9
IsmaDevs <chavezpedrito04@gmail.com>
2025-02-03 09:37:50 -0300

    uploaded files

--- src/Components/Layouts/Header.jsx
+++ src/Components/Layouts/Header.jsx
@@ -1,4 +1,4 @@
-import LogoNakama from '../../assets/Logo1.jpg'
+// import LogoNakama from '../../assets/Logo1.jpg'
 
 const Header = () => {
 return (
@@ -10,7 +10,7 @@ return (
                 <p className='text-xl text-white'>Software</p>
             </div>
             <div>
-                <button className="bg-[#095ca4] border-2 border-blue-600 rounded-full p-2 px-4 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-[#095ca4] cursor-pointer">
+                <button className="bg-[#095ca4] border-2 border-blue-600 rounded-full text-white p-2 px-4 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-[#095ca4] cursor-pointer">
                     Contacto
                 </button>
             </div>
--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -2,22 +2,22 @@ const Services = () => {
     return (
         <div className="p-4">
             <div className="flex flex-col items-center justify-center">
-                <p className="text-3xl sm:text-5xl text-[#00275c] font-bold">
+                <p className="text-3xl sm:text-5xl text-[#00275c] font-bold font-[Public-Sans]">
                     Nuestros <span className="text-white">servicios</span>
                 </p>
             </div>
             <div className="mt-8 flex flex-col items-center m-2 sm:m-5">
                 <div className="flex flex-col gap-4 w-full max-w-md">
-                    <div className="bg-[#00275c] text-white p-4 rounded-lg transform transition-transform duration-300 hover:scale-105 cursor-pointer">
+                    <div className="bg-[#00275c] text-white font-[Public-Sans] p-4 rounded-lg transform transition-transform duration-300 hover:scale-105 cursor-pointer">
                         <h2 className="text-xl sm:text-2xl font-semibold">Desarrollo web</h2>
                     </div>
-                    <div className="bg-[#00275c] text-white p-4 rounded-lg transform transition-transform duration-300 hover:scale-105 cursor-pointer">
+                    <div className="bg-[#00275c] text-white font-[Public-Sans] p-4 rounded-lg transform transition-transform duration-300 hover:scale-105 cursor-pointer">
                         <h2 className="text-xl sm:text-2xl font-semibold">Desarrollo de aplicaciones</h2>
                     </div>
-                    <div className="bg-[#00275c] text-white p-4 rounded-lg transform transition-transform duration-300 hover:scale-105 cursor-pointer">
+                    <div className="bg-[#00275c] text-white font-[Public-Sans] p-4 rounded-lg transform transition-transform duration-300 hover:scale-105 cursor-pointer">
                         <h2 className="text-xl sm:text-2xl font-semibold">Sistemas de gestión</h2>
                     </div>
-                    <div className="bg-[#00275c] text-white p-4 rounded-lg transform transition-transform duration-300 hover:scale-105 cursor-pointer">
+                    <div className="bg-[#00275c] text-white font-[Public-Sans] p-4 rounded-lg transform transition-transform duration-300 hover:scale-105 cursor-pointer">
                         <h2 className="text-xl sm:text-2xl font-semibold">E-Commerce</h2>
                     </div>
                 </div>
